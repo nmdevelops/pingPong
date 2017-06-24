69af36f53dd6d3936c381e39761c01ffbf9d163b
Nathan Mayer <nm@gmail.com>
2017-06-24 15:49:01 -0700

    Add css styling

--- js/scripts.js
+++ js/scripts.js
@@ -10,29 +10,24 @@ var formReset = (function () {
   $(".result ul").empty();
   $(".result").hide();
 })
-
 var pingPong = (function () {
-
   for (var i = 1; i <= userInput; i++) {
     numbers.push(i);
   }
-
   outputs = numbers.slice();
-    for (var index = 0; index <= outputs.length; index +=1) {
-
-      if (outputs [index] % 5 === 0 && outputs [index] % 3 === 0) {
-        outputs [index] = "Ping pong!"
-      } else if (outputs [index] % 5 === 0) {
-        outputs [index] = "pong"
-      } else if (outputs [index] % 3 === 0) {
-        outputs [index] = "ping"
-      }
+  for (var index = 0; index <= outputs.length; index +=1) {
+    if (outputs [index] % 5 === 0 && outputs [index] % 3 === 0) {
+      outputs [index] = "Ping pong!"
+    } else if (outputs [index] % 5 === 0) {
+      outputs [index] = "pong"
+    } else if (outputs [index] % 3 === 0) {
+      outputs [index] = "ping"
     }
-    $(".result").show();
-
-    outputs.forEach(function(output) {
-          $(".result ul").append("<li>" + output + "</li>");
-    })
+  }
+  $(".result").show();
+  outputs.forEach(function(output) {
+    $(".result ul").append("<li>" + output + "</li>");
+  })
 })
 //front-end
 $(document).ready(function (){
